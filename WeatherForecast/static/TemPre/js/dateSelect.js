30c3e081d46d085660af3401eda52184cef927c5
FantaPi <2564305544@qq.com>
2020-07-31 14:27:47 +0800

    小问题修改及功能优化

--- WeatherForecast/static/TemPre/js/dateSelect.js
+++ WeatherForecast/static/TemPre/js/dateSelect.js
@@ -1,4 +1,4 @@
-function initSelect(){
+function initSelect(sel_year, sel_month){
     var y = new Date().getFullYear();
     var year_list = "";
     for(var i=2012; i<=y; i++){
@@ -8,7 +8,7 @@ function initSelect(){
             year_list += "<option value=" + i + ">" + i + "</option>";
         }
     }
-    $("#sel_year").append(year_list);
+    $("#"+sel_year).append(year_list);
 
     var m = new Date().getMonth() + 1;
     console.log(m)
@@ -20,19 +20,21 @@ function initSelect(){
             month_list += "<option value=" + i + ">" + i + "</option>";
         }
     }
-    $("#sel_month").append(month_list);
+    $("#"+sel_month).append(month_list);
 }
 
-function onchangeSelect(){
-    $("#sel_year, #sel_month").change(function(){
-        var yearSel = $("#sel_year").val();
-        var monthSel = $("#sel_month").val();
-        nowMonth = new Date().getMonth() + 1
-        if(monthSel > nowMonth){
-            drawPic1(yearSel,nowMonth);
+function onchangeSelect(sel_year,sel_month){
+    $("#" + sel_year + ", #" + sel_month).change(function(){
+        var yearSel = $("#" + sel_year).val();
+        var monthSel = $("#" + sel_month).val();
+        var nowYear = new Date().getFullYear();
+        var nowMonth = new Date().getMonth() + 1
+        if(monthSel > nowMonth && yearSel >= nowYear){
+            $("#" + sel_month).val(nowMonth);
+            drawHistoryPic(yearSel,nowMonth);
             alert('请选择当前月份之前的时间！')
         }else{
-            drawPic1(yearSel,monthSel);
+            drawHistoryPic(yearSel,monthSel);
         }
     });
 }
--- WeatherForecast/static/TemPre/js/processData.js
+++ WeatherForecast/static/TemPre/js/processData.js
@@ -1,19 +1,20 @@
 $(document).ready(function(){
-    initSelect();
-    drawPic1(new Date().getFullYear(),new Date().getMonth() + 1);
-    onchangeSelect();
+    initSelect("sel_year", "sel_month");
+    drawHistoryPic(new Date().getFullYear(),new Date().getMonth() + 1);
+    onchangeSelect("sel_year", "sel_month");
 });
 
-function drawPic1(year, month){
+function drawHistoryPic(year, month){
     var myChart = echarts.init(document.getElementById('pic1'));
     myChart.showLoading();
     // 异步加载数据
     $.get('getMonthlyData/?year=' + year + '&month=' + month).done(function (data) {
-        var t = "月份:" + data.date + " 城市:" + data.city;
+        var t = "月份:" + year + "-" + month + " 城市:" + data.city;
         max = Number(data["maxWendu"].split("（")[0]);
         maxDate = data["maxWendu"].split("（")[1].split("）")[0];
         min = Number(data["minWendu"].split("（")[0]);
         minDate = data["minWendu"].split("（")[1].split("）")[0];
+        
         myChart.hideLoading();
         myChart.setOption({
             title: {
