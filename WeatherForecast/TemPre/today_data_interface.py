c6e1a481b5a212c90ff1881b51d632d6d637a4be
1250687304 <1250687304@qq.com>
2020-08-01 13:09:30 +0800

    Update today_data_interface.py

--- WeatherForecast/TemPre/today_data_interface.py
+++ WeatherForecast/TemPre/today_data_interface.py
@@ -10,12 +10,12 @@ def getTodayData(request):
     @return 今天的天气数据json
     @tips 请用GET访问
     """
-    print("1")
+
     if request.method == "GET":
-        print("1")
         filepath = STATICFILES_DIRS[0] + "/data/今日数据.json"
         with open(filepath, "r", encoding="utf-8") as f:
             data = json.load(f)
             return JsonResponse(data)
     else:
         return JsonResponse("{'msg': '请用GET方法进行访问！'}")
+
